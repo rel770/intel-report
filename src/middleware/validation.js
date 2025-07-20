d81aadfec14fe79bc9a94f96be65bf9f5563cd36
rel700 <rel@mail.com>
2025-07-20 07:21:31 +0300

    feat: add ObjectId validation middleware

---
+++ src/middleware/validation.js
@@ -0,0 +1,22 @@
+/**
+ * Validation Middleware
+ * Validates ObjectId format and sanitizes input
+ */
+
+const { ObjectId } = require("mongodb");
+const { ApiError } = require("./errorHandler");
+
+/**
+ * Validates MongoDB ObjectId format
+ */
+const validateObjectId = (paramName = "id") => {
+  return (req, res, next) => {
+    const id = req.params[paramName];
+
+    if (!ObjectId.isValid(id)) return next(new ApiError(400, `Invalid ${paramName} format`));
+
+    next();
+  };
+};
+
+module.exports = { validateObjectId };
