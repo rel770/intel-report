a1a9379b16ee6e3b3addc0cad7238504d86e9974
rel700 <rel@mail.com>
2025-07-20 07:55:52 +0300

    feat: add Joi validateBody middleware

--- src/middleware/validation.js
+++ src/middleware/validation.js
@@ -18,6 +18,26 @@ const validateObjectId = (paramName = "id") => {
   };
 };
 
+/**
+ * Validates request body against Joi schema
+ */
+const validateBody = (schema) => {
+  return (req, res, next) => {
+    const { error, value } = schema.validate(req.body, {
+      abortEarly: false, // Validate all fields, not just the first error
+      stripUnknown: true // Remove unknown fields
+    });
+
+    if (error) {
+      return next(error);
+    }
+
+    req.body = value;
+    next();
+  };
+};
+
 module.exports = {
-  validateObjectId
+  validateObjectId,
+  validateBody
 };
